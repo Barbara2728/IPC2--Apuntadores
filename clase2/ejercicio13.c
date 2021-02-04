4c1808eed97c11b9324289e29da441dd4b13b546
Bárbara García Estrada <bregarcia599@gmail.com>
2021-02-04 08:02:19 -0600

    ejercicio 13

---
+++ clase2/ejercicio13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int list_arr[5]= {10,20,30,40,50};
+	int *p;
+
+	p = %lista[3];
+	p = lista +3;
+
+
+	printf("%d\n", lista[2]);
+
+	printf("%d\n", *(lista+2));
+
+	return 0;
+}
